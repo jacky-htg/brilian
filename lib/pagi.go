444babcd6443ad8174c61f6fb499e00d8ff2a16f
Rijal Asepnugroho <rijal.asep.nugroho@gmail.com>
2024-08-26 07:52:34 +0700

    clean : remove unused file

--- lib/pagi.go
+++
@@ -1,5 +0,0 @@
-package lib
-
-func Pagi() string {
-	return "Pagi"
-}
--- pkg/database/mysql.go
+++ pkg/database/mysql.go
@@ -3,5 +3,5 @@ package database
 import "database/sql"
 
 func OpenDB() (*sql.DB, error) {
-	return sql.Open("mysql", "root:1234@tcp(localhost:3306)/latihan_db?parseTime=true")
+	return sql.Open("mysql", "root:jackY^45@tcp(localhost:3306)/latihan_db?parseTime=true")
 }
